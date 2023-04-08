cb900cc363d8b169b50a1020da00fd36c1374a7e
seungm2n <seungm2n2@gmail.com>
2023-04-08 17:52:18 +0900

    mod

--- src/App.js
+++ src/App.js
@@ -6,12 +6,26 @@ import './App.css';
 
 // Main
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'World Wide Web!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="World Wide Web !"></Subject>
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}>
+        </Subject>
         <Subject title="React" sub="For UI"></Subject>
-        <TOC></TOC>
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is Hyper Text Markup Language."></Content>
       </div>
     );
