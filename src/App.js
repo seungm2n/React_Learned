f9e070e1e8d19056e9ec36f714365a09db87cbbe
seungm2n <seungm2n2@gmail.com>
2023-04-08 13:00:23 +0900

    refactoring

--- src/App.js
+++ src/App.js
@@ -5,28 +5,51 @@ class Subject extends Component{
   render() {
     return (
       <header>
-        <h1>WEB</h1>
-        World Wide Web !
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
       </header>
     );
   }
 }
+
+class TOC extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li><a href='1.html'>HTML</a></li>
+          <li><a href='2.html'>CSS</a></li>
+          <li><a href='3.html'>React</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <article>
+        <h2>HTML</h2>
+        HTML is Hyper Text Markup Language.
+      </article>
+    );
+  }
+}
+
+// Main
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Subject></Subject>
+        <Subject title="WEB" sub="World Wide Web !"></Subject>
+        <TOC></TOC>
+        <Content></Content>
       </div>
     );
   }
 }
-class TOC extends Component {
-  render() {
-    return (
-      <nav></nav>
-    );
-  }
-}
+
 
 
 export default App;
