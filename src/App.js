65f695016c9fd58fd3f79025c6442f79495127e0
seungm2n <seungm2n2@gmail.com>
2023-04-08 23:02:16 +0900

    ,

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mode:'welcome',
+      mode:'read',
+      selected_content_id:2,
       subject:{title:'WEB', sub:'World Wide Web !'},
       welcome:{title:'Welcome', desc:'Hello, React !!'},
       contents:[
@@ -26,18 +27,30 @@ class App extends Component {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
     } else if (this.state.mode === 'read'){
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      var i = 0;
+      while(i < this.state.contents.length) {
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id){
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1
+      } 
     }
     console.log('render',this);
     return (
       <div className="App">
-        {/* <Subject 
+        <Subject 
           title={this.state.subject.title} 
-          sub={this.state.subject.sub}>
-        </Subject> */}
+          sub={this.state.subject.sub}
+          onChangePage={function(){
+            this.setState({mode:'welcome'});
+          }.bind(this)}
+        >
+        </Subject>
 
-        <header>
+        {/* <header>
           <h1><a href="/" onClick={function(e){
             console.log(e);
             e.preventDefault();
@@ -47,9 +60,14 @@ class App extends Component {
             });
             }.bind(this)}>{this.state.subject.title}</a></h1>
           {this.state.subject.sub}
-        </header> 
+        </header> */} 
 
-        <TOC data={this.state.contents}></TOC>
+        <TOC onChangePage={function(id){
+          this.setState({
+            mode:'read',
+            selected_content_id:Number(id)
+          });
+        }.bind(this)}data={this.state.contents}></TOC>
         <Content title={_title} desc={_desc}></Content>
 
       </div>
